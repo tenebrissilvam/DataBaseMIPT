dec1f4a274d31818a2a1051a7ad3ec852390589d
tenebrissilvam <bakaeva.ea@phystech.edu>
2023-04-20 20:53:23 +0300

    add vault_basic_queries.sql

---
+++ scripts/vault_basic_queries.sql
@@ -0,0 +1,32 @@
+--CRUD - Sreate, Read, Update, Delete
+
+--Create:
+
+insert into cd.person (person_id, surname, firstname, responsibility, radiation_level, taking_medicine, consuming_prov, pet_id, stamina)
+values (600, 'Wright', 'Edgar', 'Handyman', 80, 6, 87, 14, 36);
+
+insert into cd.animal (anim_id, pet_name, animal_type, fighting_strength)
+values (601, 'Hachiko', 'dog', 15);
+
+insert into cd.medicine (med_id, med_type, restores_rad, med_cnt)
+values (602, 'X-cell', 65, 20);
+
+--Read:
+
+select surname, firstname from cd.person p where responsibility = 'Mercenary';
+
+select med_type from cd.medicine m where restores_rad > 100;
+
+select prov_type from cd.provisions p where nutritional_val > 100 and nutritional_val < 180;
+
+--Update:
+
+update cd.medicine set med_cnt = 100 where med_id = 2;
+
+update cd.animal set fighting_strength = 55 where animal_type = 'cat' and fighting_strength < 55;
+
+--Delete:
+
+delete from cd.person where radiation_level > 90;
+
+delete from cd.bedroom where bedroom_id = 105;
